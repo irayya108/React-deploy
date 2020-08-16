740ca603210e2b682057d7fc883e6adc8a150d8f
irayya108 <irayya@wiznet.io>
2020-08-16 15:25:31 +0530

    Updates

---
+++ precache-manifest.6d57304a368ecc38db978524e80a7bb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c41319715bbab3c27ef1235e58f112d",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "3e13486324739919379e",
+    "url": "/react-deploy/static/css/main.ae75b94a.chunk.css"
+  },
+  {
+    "revision": "c69c894df23546079913",
+    "url": "/react-deploy/static/js/2.92b1f7a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.92b1f7a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e13486324739919379e",
+    "url": "/react-deploy/static/js/main.ef94d069.chunk.js"
+  },
+  {
+    "revision": "23d02aab4290084fc16b",
+    "url": "/react-deploy/static/js/runtime-main.668f5768.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
